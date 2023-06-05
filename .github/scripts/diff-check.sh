3fb46dfc439aa0e6c701530ad5c88e6e89d27cd1
Marshall Ku <marshall@kakao.com>
2023-06-06 03:13:28 +0900

    Fix regex to find solution files

--- .github/scripts/diff-check.sh
+++ .github/scripts/diff-check.sh
@@ -3,7 +3,7 @@ github_sha="$1"
 diff_output=$(git diff-tree --no-commit-id --name-only -r "$github_sha")
 readarray -t diffs <<<"$diff_output"
 
-file_pattern='solutions/[0-9]+/solution\.(py|cpp)'
+file_pattern='[0-9]+/solution\.(py|cpp)'
 
 for file in "${diffs[@]}"; do
     if [[ "$file" =~ $file_pattern ]]; then
